bb9a76431c136965ace73cf5d45633c780c24cfc
Nostro37 <nostro37@gmail.com>
2022-02-19 10:09:59 -0500

    app changed from function to class

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -7,102 +7,112 @@ import Particles from "react-tsparticles";
 
 import './App.css';
 
-const particlesOptions = {
 
-}
+class App extends Component() {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-    <Particles 
-    id="tsparticles"
-    options={{
-      background: {
-        color: {
-          value: "none"
-        }
-      },
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: true,
-            mode: "push"
-          },
-          onHover: {
-            enable: true,
-            mode: "repulse"
-          },
-          resize: true
-        },
-        modes: {
-          bubble: {
-            distance: 400,
-            duration: 2,
-            opacity: 0.8,
-            size: 40
-          },
-          push: {
-            quantity: 4
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4
+  onInputChange = (event) => {
+    console.log(event);
+  }
+
+  render(){
+    return (
+      <div className="App">
+      <Particles 
+      id="tsparticles"
+      options={{
+        background: {
+          color: {
+            value: "none"
           }
-        }
-      },
-      particles: {
-        color: {
-          value: "#ffffff"
         },
-        links: {
-          color: "#ffffff",
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1
-        },
-        collisions: {
-          enable: true
-        },
-        move: {
-          direction: "none",
-          enable: true,
-          outMode: "bounce",
-          random: false,
-          speed: 3,
-          straight: false,
-          bounce: false
-        },
-        number: {
-          density: {
-            enable: true,
-            area: 800
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push"
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse"
+            },
+            resize: true
           },
-          value: 80
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40
+            },
+            push: {
+              quantity: 4
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4
+            }
+          }
         },
-        opacity: {
-          value: 0.5
+        particles: {
+          color: {
+            value: "#ffffff"
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1
+          },
+          collisions: {
+            enable: true
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 1,
+            straight: false,
+            bounce: false
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800
+            },
+            value: 80
+          },
+          opacity: {
+            value: 0.5
+          },
+          shape: {
+            type: "circle"
+          },
+          size: {
+            random: true,
+            value: 5
+          }
         },
-        shape: {
-          type: "circle"
-        },
-        size: {
-          random: true,
-          value: 5
-        }
-      },
-      detectRetina: true
-    }} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    {/* {
-    
-    <FaceRecognition />} */}
-    </div>
-  );
-}
+        detectRetina: true
+      }} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} />
+      {/* {
+      <FaceRecognition />} */}
+      </div>
+    );
+  }
+  }
+  
 
 export default App;
