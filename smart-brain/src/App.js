e91dd7f5c374ab3b074126883524885fdf0a154b
Nostro37 <nostro37@gmail.com>
2022-02-22 11:33:37 -0500

    waiting, this part really seems to mess people up.

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -3,8 +3,6 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import Particles from "react-tsparticles";
-
 import './App.css';
 
 
@@ -17,96 +15,23 @@ class App extends Component() {
   }
 
   onInputChange = (event) => {
-    console.log(event);
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
   }
 
   render(){
     return (
       <div className="App">
-      <Particles 
-      id="tsparticles"
-      options={{
-        background: {
-          color: {
-            value: "none"
-          }
-        },
-        fpsLimit: 60,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push"
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse"
-            },
-            resize: true
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40
-            },
-            push: {
-              quantity: 4
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4
-            }
-          }
-        },
-        particles: {
-          color: {
-            value: "#ffffff"
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1
-          },
-          collisions: {
-            enable: true
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 2,
-            straight: false,
-            bounce: false
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800
-            },
-            value: 80
-          },
-          opacity: {
-            value: 0.5
-          },
-          shape: {
-            type: "circle"
-          },
-          size: {
-            random: true,
-            value: 5
-          }
-        },
-        detectRetina: true
-      }} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} />
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}
+        />
       {/* {
       <FaceRecognition />} */}
       </div>
